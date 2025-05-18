c562ae375fa4bae65126773242857866a31254d9
Your Name <you@example.com>
2025-05-18 10:40:42 +0100

    GitHubActions test

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__) 
 @app.route('/')
  def hello(): 
-return "Hello from Docker!" 
+return "Hello from  hamza!" 
 if __name__ == "__main__": 
     app.run(host='0.0.0.0', port=5000)
